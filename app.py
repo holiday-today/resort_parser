a0fbc06fe3a22ab78134d7a9eb795d2e7169088e
holiday-today <108573171+holiday-today@users.noreply.github.com>
2022-08-05 23:14:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,36 @@ logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
+def to_main_json(data):
+    post_json = {
+        'STATEINC': None,           
+        'CHECKIN_BEG': None,  
+        'CHECKIN_END': None,  
+        'NIGHTS_FROM': None,         
+        'NIGHTS_TILL': None,         
+        'ADULT': None,               
+        'CHILD': None,               
+        'AGES': [],
+        'CURRENCY': '1',            
+        'TOWNS': [],
+        'HOTELS': [],
+        'MEALS': [],
+        'STARS': [],
+        'FILTER': '1',              
+        'PRICEPAGE': 1,             
+        'DOLOAD': 1                 
+    }
+    for el in data:
+        if post_json[el] is None:
+            post_json[el] = data[el]
+        else:
+            post_json[el].append(data[el])
+    return post_json
+
 @app.route("/", methods=["POST"])
 def test():
     try:
         f = request.json
-        print('First method!')
     except Exception as e1:
-        try:
-            f = request.get_json(force=True)
-            print('Second method!')
-        except Exception as e2:
-            try:
-                f = request.get_json(force=True)
-                print('Third method!')
-            except Exception as e3:
-                return 'Failed!'
-    return f
+        return 'Bad response json'
+    return to_main_json(f)
