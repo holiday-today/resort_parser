e03216639f1aead0ce3e49006908cedc68934103
holiday-today <108573171+holiday-today@users.noreply.github.com>
2022-08-05 22:38:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,31 +5,31 @@ import json
 
 app = Flask(__name__)
 
-def to_json(data):
-    post_json = {
-        'STATEINC': None,           
-        'CHECKIN_BEG': None,  
-        'CHECKIN_END': None,  
-        'NIGHTS_FROM': None,         
-        'NIGHTS_TILL': None,         
-        'ADULT': None,               
-        'CHILD': None,               
-        'AGES': [],
-        'CURRENCY': '1',            
-        'TOWNS': [],
-        'HOTELS': [],
-        'MEALS': [],
-        'STARS': [],
-        'FILTER': '1',              
-        'PRICEPAGE': 1,             
-        'DOLOAD': 1                 
-    }
-    for el in data:
-        if post_json[el] is None:
-            post_json[el] = data[el]
-        else:
-            post_json[el].append(data[el])
-    return post_json
+# def to_json(data):
+#     post_json = {
+#         'STATEINC': None,           
+#         'CHECKIN_BEG': None,  
+#         'CHECKIN_END': None,  
+#         'NIGHTS_FROM': None,         
+#         'NIGHTS_TILL': None,         
+#         'ADULT': None,               
+#         'CHILD': None,               
+#         'AGES': [],
+#         'CURRENCY': '1',            
+#         'TOWNS': [],
+#         'HOTELS': [],
+#         'MEALS': [],
+#         'STARS': [],
+#         'FILTER': '1',              
+#         'PRICEPAGE': 1,             
+#         'DOLOAD': 1                 
+#     }
+#     for el in data:
+#         if post_json[el] is None:
+#             post_json[el] = data[el]
+#         else:
+#             post_json[el].append(data[el])
+#     return post_json
 
 
 @app.route("/get/allresult", methods=["POST"])
