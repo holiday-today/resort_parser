ffc0d490e5b86c0b13c7fd19b0d0b160dbc104d5
holiday-today <108573171+holiday-today@users.noreply.github.com>
2022-08-05 22:42:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
-import main
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
 
-# def to_json(data):
-#     post_json = {
-#         'STATEINC': None,           
-#         'CHECKIN_BEG': None,  
-#         'CHECKIN_END': None,  
-#         'NIGHTS_FROM': None,         
-#         'NIGHTS_TILL': None,         
-#         'ADULT': None,               
-#         'CHILD': None,               
-#         'AGES': [],
-#         'CURRENCY': '1',            
-#         'TOWNS': [],
-#         'HOTELS': [],
-#         'MEALS': [],
-#         'STARS': [],
-#         'FILTER': '1',              
-#         'PRICEPAGE': 1,             
-#         'DOLOAD': 1                 
-#     }
-#     for el in data:
-#         if post_json[el] is None:
-#             post_json[el] = data[el]
-#         else:
-#             post_json[el].append(data[el])
-#     return post_json
+def to_json(data):
+    post_json = {
+        'STATEINC': None,           
+        'CHECKIN_BEG': None,  
+        'CHECKIN_END': None,  
+        'NIGHTS_FROM': None,         
+        'NIGHTS_TILL': None,         
+        'ADULT': None,               
+        'CHILD': None,               
+        'AGES': [],
+        'CURRENCY': '1',            
+        'TOWNS': [],
+        'HOTELS': [],
+        'MEALS': [],
+        'STARS': [],
+        'FILTER': '1',              
+        'PRICEPAGE': 1,             
+        'DOLOAD': 1                 
+    }
+    for el in data:
+        if post_json[el] is None:
+            post_json[el] = data[el]
+        else:
+            post_json[el].append(data[el])
+    return post_json
 
 
 @app.route("/get/allresult", methods=["POST"])
 def test():
     f = to_json(request.form)
-    return f
+    return 
