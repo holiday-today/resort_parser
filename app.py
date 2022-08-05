5c86484a2a40e3dd15c34d567ea1481d0b0f9535
holiday-today <108573171+holiday-today@users.noreply.github.com>
2022-08-05 23:02:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,16 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def test():
     try:
-        f = request.form
+        f = request.json
+        print('First method!')
     except Exception as e1:
         try:
             f = request.get_json(force=True)
+            print('Second method!')
         except Exception as e2:
-            return 'Failed 2'
+            try:
+                f = request.get_json(force=True)
+                print('Third method!')
+            except Exception as e3:
+                return 'Failed!'
     return f
