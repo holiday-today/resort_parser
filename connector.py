ca2f55d915b611a6df1d0426fd5cfdbf4290652a
holiday-today <108573171+holiday-today@users.noreply.github.com>
2022-08-09 01:49:07 +0300

    Update connector.py

--- connector.py
+++ connector.py
@@ -6,24 +6,30 @@ def sres(a, b):
 	c = set(a) & set(b)
 	return len(c)
 
-def chng(mylist):
-	newlist = []
-	for x in mylist:
-		t = x.lower().replace('one', '1')
-		t = t.lower().replace('two', '2')
-		t = t.lower().replace('three', '3')
-		newlist.append(t)
-	return newlist
+def chng(x):
+	if 'one' in x.lower():
+		return x.replace('One', '1').replace('one', '1')
+	elif 'two' in x.lower():
+		return x.replace('Two', '1').replace('two', '1')
+	elif 'three' in x.lower():
+		return x.replace('Three', '1').replace('three', '1')
+	elif 'four' in x.lower():
+		return x.replace('Four', '1').replace('four', '1')
+	elif 'five' in x.lower():
+		return x.replace('Five', '1').replace('five', '1')
+	elif 'six' in x.lower():
+		return x.replace('Six', '1').replace('six', '1')
+	else:
+		return x
+	#return x.replace('One', '1').replace('one', '1').replace('Two', '2').replace('two', '2').replace('Three', '3').replace('three', '3').replace('Four', '4').replace('four', '4').replace('Five', '5').replace('five', '5')
 
 def connect(list1, list2, key):
 	list3 = []
-	list1 = chng(list1)
-	list2 = chng(list2)
 	for i in list1:
 		max_q = 0
 		q_id = []
 		for j in list2:
-			q = sres(i[key], j)
+			q = sres(chng(i[key]), chng(j))
 			if q > max_q:
 				q_id = [j]
 				max_q = q
@@ -35,7 +41,7 @@ def connect(list1, list2, key):
 		max_q = 0
 		q_id = []
 		for j in i[1]:
-			q = sres(i[0][key], j)
+			q = sres(chng(i[0][key]), chng(j))
 			if q > max_q:
 				q_id = [j]
 				max_q = q
@@ -46,15 +52,13 @@ def connect(list1, list2, key):
 			min_q = 99
 			s = []
 			for j in q_id:
-				q = len(re.split(' |-', j))-sres(i[0][key], j)
+				q = len(re.split(' |-', j))-sres(chng(i[0][key]), chng(j))
 				if q < min_q:
 					min_q = q
 					s = [j]
 				elif q == min_q:
 					s.append(j)
 			q_id = s
-			if len(q_id)>1:
-				q_id = []
 		i[1] = q_id
 		#else:
 		#	i[1] = []
