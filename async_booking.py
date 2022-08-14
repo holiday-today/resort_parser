3e5b45bbc9f37eab0ed439fc626ce18d8a8acdc5
holiday-today <108573171+holiday-today@users.noreply.github.com>
2022-08-14 12:10:32 +0300

    Update async_booking.py

--- async_booking.py
+++ async_booking.py
@@ -230,7 +230,7 @@ def my_async(hh, func):
     b = []
 
     for i in range(0, lh//2, lh//p):
-        b.append(Process(target=func, args=([hh[i:min(i+lh//p, lh//2)], soups, tt])))
+        b.append(Process(target=func, args=([hh[i:min(i+lh//p, lh//2)], soups])))
     for i in b:
         i.start()
     for i in b:
@@ -239,7 +239,7 @@ def my_async(hh, func):
     b = []
     print('1/2:', time.time()-tt)
     for i in range(lh//2, lh, lh//p):
-        b.append(Process(target=func, args=([hh[i:min(i+lh//p, lh)], soups, tt])))
+        b.append(Process(target=func, args=([hh[i:min(i+lh//p, lh)], soups])))
     for i in b:
         i.start()
     for i in b:
